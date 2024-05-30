b4309c5966f0cdb35897de8e19238deefd4a6fb5
Roniel Contreras <ronielcontreras70@gmail.com>
2024-05-30 16:34:16 -0500

    Replace HelloWorld component usage by a loading page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,23 @@
 <template>
-  <HelloWorld />
+  <v-container class="fill-height">
+    <v-responsive>
+
+      <v-row justify="center">
+        <v-col cols="3">
+          <v-img src="@/assets/laying_starfruit_cookie.webp"/>
+        </v-col>
+      </v-row>
+
+      <div class="py-4"/>
+
+      <div class="text-center">
+        <div class="text-body-1 font-weight-light mb-n1">
+          Under Construction...
+        </div>
+      </div>
+
+    </v-responsive>
+  </v-container>
 </template>
 
 <script lang="ts" setup>
