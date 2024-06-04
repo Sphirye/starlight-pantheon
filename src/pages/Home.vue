d17d79569a7217e520535a03deb3aeb10671e8ac
Roniel Contreras <ronielcontreras70@gmail.com>
2024-06-03 21:56:39 -0500

    Add parallax image in home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,55 +1,63 @@
 <template>
-    <v-container >
-      <v-responsive>
-  
-        <v-row justify="end">
-          <v-col cols="5" class="d-flex flex-column align-center justify-center">
-            
-  
-            <div class="text-center text-no-wrap cookie-run-font">
-              Who is Starfruit Cookie?
-            </div>
-            
-            <div>
-                <a href="https://cookierun.fandom.com/wiki/Starfruit_Cookie" target="_blank">Starfruit Cookie</a> is the kindest 
-                and purest High Oracle the Starlight Panteon has ever meet! She has been blessed with an lucid inmense talent to read
-                the messages from the stars.
+    <v-parallax src="@/assets/hill_stars_lobby.webp" class="background-image">
+        <v-container>
+            <v-responsive>
                 
-                She will always ensures that anyone who visits the temple receives a fair and warm prediction directly from the
-                sky constellations!
-            </div>
+                <v-row justify="end">
+                    <v-col cols="5" class="d-flex flex-column align-center justify-center">
+                    
 
-            <br class="my-8">
+                    <div class="text-center text-no-wrap cookie-run-font">
+                        Who is Starfruit Cookie?
+                    </div>
+                    
+                    <div>
+                        <a href="https://cookierun.fandom.com/wiki/Starfruit_Cookie" target="_blank">Starfruit Cookie</a> is the kindest 
+                        and purest High Oracle the Starlight Panteon has ever meet! She has been blessed with an lucid inmense talent to read
+                        the messages from the stars.
+                        
+                        She will always ensures that anyone who visits the temple receives a fair and warm prediction directly from the
+                        sky constellations!
+                    </div>
 
-            <div class="text-center text-no-wrap cookie-run-font">
-              What is the Starlight Pantheon?
-            </div>
+                    <br class="my-8">
 
-            <div>
-                The Starlight Pantheon is a sacred giant sanctuary where blessings flow like water only when being guided by the
-                right cookie. Its specially know by it strong bond with the stars constellations, which riches up the clarividence
-                range when it comes to read and receive messages from the stars.
+                    <div class="text-center text-no-wrap cookie-run-font">
+                        What is the Starlight Pantheon?
+                    </div>
 
-                <br class="mb-4">
+                    <div>
+                        The Starlight Pantheon is a sacred giant sanctuary where blessings flow like water only when being guided by the
+                        right cookie. Its specially know by it strong bond with the stars constellations, which riches up the clarividence
+                        range when it comes to read and receive messages from the stars.
 
-                If you stay enough, you will be able to stare stars moving in pairs during the sunset just like
-                they were dancing!
+                        <br class="mb-4">
 
-                <br class="mb-4">
-                
-                May Starfruit Cookie find anyone to dance with, too...?
-            </div>
+                        If you stay enough, you will be able to stare stars moving in pairs during the sunset just like
+                        they were dancing!
+
+                        <br class="mb-16">
+                        
+                        May Starfruit Cookie find anyone to dance with, too...?
+                    </div>
 
 
-          </v-col>
-          <v-divider thickness="10" color="success" class="border-opacity-100"></v-divider>
+                    </v-col>
+                    <v-divider thickness="10" color="success" class="border-opacity-100"></v-divider>
 
-        </v-row>
-  
-      </v-responsive>
-    </v-container>
+                </v-row>
+
+            </v-responsive>
+        </v-container>
+    </v-parallax>
   </template>
   
   <script lang="ts" setup>
     //
   </script>
+
+<style>
+.background-image img {
+    filter: blur(4px);
+}
+</style>
